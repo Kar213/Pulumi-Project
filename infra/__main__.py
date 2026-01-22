d70a7c750236c85c05d5a4d91292a011c6ba9829
kar213 <k9301340199@gmail.com>
2026-01-22 14:23:48 +0530

    added .trivyignore file

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,36 +1,67 @@
-# import pulumi
-# from pulumi_kubernetes.apps.v1 import Deployment
-# from pulumi_kubernetes.core.v1 import Service
-
-# labels = {"app": "pulumi-demo"}
-
-# Deployment(
-#     "demo-app",
-#     spec={
-#         "replicas": 1,
-#         "selector": {"matchLabels": labels},
-#         "template": {
-#             "metadata": {"labels": labels},
-#             "spec": {
-#                 "containers": [{
-#                     "name": "app",
-#                     "image": "pulumi-demo:latest",
-#                     "ports": [{"containerPort": 5000}]
-#                 }]
-#             }
-#         }
-#     }
-# )
-
-# Service(
-#     "demo-svc",
-#     spec={
-#         "type": "NodePort",
-#         "selector": labels,
-#         "ports": [{"port": 80, "targetPort": 5000}]
-#     }
-# )
 import pulumi
-from pulumi_kubernetes.core.v1 import Namespace
+from pulumi_kubernetes.core.v1 import Namespace, Service
+from pulumi_kubernetes.apps.v1 import Deployment
 
-Namespace("demo-ns")
+# Create Namespace
+ns = Namespace(
+    "demo-ns",
+    metadata={
+        "name": "demo-ns"
+    }
+)
+
+labels = {
+    "app": "pulumi-demo"
+}
+
+# Deployment
+Deployment(
+    "pulumi-demo-deployment",
+    metadata={
+        "namespace": ns.metadata["name"]
+    },
+    spec={
+        "replicas": 1,
+        "selector": {
+            "matchLabels": labels
+        },
+        "template": {
+            "metadata": {
+                "labels": labels
+            },
+            "spec": {
+                "containers": [
+                    {
+                        "name": "app",
+                        "image": "pulumi-demo:latest",
+
+                        # 🔴 THIS LINE FIXES ImagePullBackOff
+                        "imagePullPolicy": "IfNotPresent",
+
+                        "ports": [
+                            {"containerPort": 5000}
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+)
+
+# Service
+Service(
+    "pulumi-demo-service",
+    metadata={
+        "namespace": ns.metadata["name"]
+    },
+    spec={
+        "type": "NodePort",
+        "selector": labels,
+        "ports": [
+            {
+                "port": 80,
+                "targetPort": 5000
+            }
+        ]
+    }
+)
